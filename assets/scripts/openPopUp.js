1932a86d6dc4b3e503d89b58419904b95f84b4f1
amaksix <31368619+amaksix@users.noreply.github.com>
2025-04-03 21:20:58 +0300

    email is set but only for testing

--- assets/scripts/openPopUp.js
+++ assets/scripts/openPopUp.js
@@ -1,7 +1,9 @@
-document.getElementById("openPopup").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "flex";
-});
 
-document.getElementById("closePopup").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "none";
+document.getElementById("closePopupSucces").addEventListener("click", function() {
+    document.getElementById("popupSucces").style.display = "none";
+    console.log("happer");
+});
+document.getElementById("closePopupError").addEventListener("click", function() {
+    console.log("happer");
+    document.getElementById("popupError").style.display = "none";
 });
